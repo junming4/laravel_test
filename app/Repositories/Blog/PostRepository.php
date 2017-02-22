cea759f71894e3bfcd0985a0a0e2832391c9b6ae
junming4 <2284876299@qq.com>
2017-02-22 15:57:31 +0800

    安装仓库控件

---
+++ app/Repositories/Blog/PostRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Blog;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface PostRepository
+ * @package namespace App\Repositories\Blog;
+ */
+interface PostRepository extends RepositoryInterface
+{
+    //
+}
